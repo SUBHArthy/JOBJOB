e0a288ade2f43ef1e42c8e1e52358cf9e6755101
SUBHArthy <maitysubharthy@gmail.com>
2023-07-10 15:52:51 +0530

    //COMMIT

---
+++ ZZZZZ/src/test/java/sample/SampleLogin.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleLogin {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://www.google.com/");
+	}
+
+}
+ 
